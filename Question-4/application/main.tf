af56652297d7b8187d21e2c5d50d642ef9c326b0
praveen chigurupati <praveenchigurupati2012@gmail.com>
2021-08-19 17:46:09 +0530

    fixing user_data

--- Question-4/application/main.tf
+++ Question-4/application/main.tf
@@ -11,17 +11,15 @@ locals {
     Name        = "app-blue"
   }
   user_data = <<EOF
-  sudo yum update -y
-  sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-  sudo yum install -y httpd
-  sudo systemctl start httpd
-  sudo systemctl enable httpd
-  sudo chown -R ec2-user:apache /var/www
-  sudo chmod 2775 /var/www
-  find /var/www -type d -exec sudo chmod 2775 {} \;
-  find /var/www -type f -exec sudo chmod 0664 {} \;
-  cd /var/www/html
-  echo "Welcome to applicatio Blue" >> index.html
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo service httpd start
+    cd /var/www/html/
+    sudo mkdir sampledir
+    sudo chown  ec2-user sampledir
+    sudo chmod -R o+r sampledir
+    cd sampledir
+    echo "<html><h1>Hello!!!</h1></html>" > hello.html
   EOF
 }
 
